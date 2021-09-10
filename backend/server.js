9ef5745652c757c1c3bb2f9aa25a07934863b240
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-09-10 09:53:10 +0200

    add connection to mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
@@ -7,7 +9,17 @@ const app = express();
 
 app.use(express.json());
 
-// Run our server
+const db = process.env.DATABASE_URL;
+
+// Connect to Mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
