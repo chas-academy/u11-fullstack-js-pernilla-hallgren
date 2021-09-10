88f86693f9627d7d6b07c357534a1ef92b9e1217
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-09-10 09:54:43 +0200

    save changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require("config");
+// const config = require("config");
 const cors = require("cors");
 
 const app = express();
