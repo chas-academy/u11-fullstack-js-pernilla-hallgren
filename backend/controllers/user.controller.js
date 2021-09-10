0e3355975f51d55f7b92588e66140631bf90b805
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-09-10 10:59:36 +0200

    add get all users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/User.model");
+const router = require("../routes/api/users");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -51,4 +52,16 @@ const register = (req, res) => {
   });
 };
 
-module.exports = register;
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  register,
+  getAllUsers,
+};
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const register = require("../../controllers/user.controller.js");
+const {
+  register,
+  getAllUsers,
+} = require("../../controllers/user.controller.js");
 
 const router = express.Router();
 
 // POST api/users - register new user
 router.post("/", register);
+
+router.get("/", getAllUsers);
 
 module.exports = router;
