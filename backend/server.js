77f882845aa0de075a322fba2c4403222a14be91
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-09-23 13:50:36 +0200

    add heroku deploy setting

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,18 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use(express.json());
 
 const db = process.env.DATABASE_URL;
--- frontend/src/shared/services/requests.js
+++ frontend/src/shared/services/requests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://u11-fullstack-app.herokuapp.com";
 
 export const GET = (url) => {
   return axios.get(`${API_URL}/${url}`, {
