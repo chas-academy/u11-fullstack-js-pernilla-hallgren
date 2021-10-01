7a2d50941a589791a75333ca694e24f98d87e3f9
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-10-01 13:02:34 +0200

    update route.rest and api url

--- frontend/src/shared/services/requests.js
+++ frontend/src/shared/services/requests.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // const API_URL = "https://u11-fullstack-app.herokuapp.com/api";
+// const API_URL = "http://localhost:5000/api"
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const GET = (url) => {
